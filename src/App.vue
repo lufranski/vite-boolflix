ee50eda9facdf54e943984413d889aea44fab67a
lufranski <lufranski@gmail.com>
2022-12-16 15:08:37 +0100

    SearchBar.vue created and imported

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script >
+  import SearchBar from './components/SearchBar.vue';
+
+  export default {
+    components: {
+      SearchBar
+    }
+  }
 </script>
 
 <template>
-  
+  <SearchBar />
 </template>
 
 <style lang="scss">
-
+  @use '../src/style/general.scss' as *;
+  @use './style/partials/variables' as *;
+  
 </style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,31 @@
+<script >
+    export default {
+
+    }
+</script>
+
+<template>
+  <div class="container">
+    <input type="text">
+
+    <div class="button">
+      Search
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+    @use '../src/style/general.scss' as *;
+
+    .container {
+        background-color: #000;
+        display: flex;
+        justify-content: space-between;
+        padding: 1rem;        
+        .button {
+            color: #fff;
+            border: 1px solid #fff;
+            border-radius: 10px;
+        }
+    }
+</style>
