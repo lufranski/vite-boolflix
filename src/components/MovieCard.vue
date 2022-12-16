833f5370362983d65023d36ff528feb860a7e683
lufranski <lufranski@gmail.com>
2022-12-16 17:16:11 +0100

    Sub-Milestone 1 - Original Title visible only if it is different from Title

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -20,7 +20,7 @@
         <li>
         
             <h3>{{movie.title}}</h3>
-            <h5>1. {{movie.original_title}}</h5>
+            <h5 v-if="movie.original_title !== movie.title">1. {{movie.original_title}}</h5>
             <h5>2. {{movie.original_language}}</h5>
             <h5>3. {{movie.vote_average}}</h5>
         
