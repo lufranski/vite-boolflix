3d1e54b8290f745c76511b653daa81e966472e71
lufranski <lufranski@gmail.com>
2022-12-16 14:49:49 +0100

    changed api url

--- src/store.js
+++ src/store.js
@@ -2,6 +2,6 @@ import { reactive } from "vue";
 
 export const store = reactive ({
     movieList: [],
-    apiUrl: 'https://rickandmortyapi.com/api/character',
+    apiUrl: 'https://api.themoviedb.org/3/search/movie?api_key=381a82bd8b02866089d3f125535bf53d&query=horror',
     movieQuery: '',
 });
