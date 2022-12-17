c612aff29cd2645e247dead26bf741bec1ed8b8b
lufranski <lufranski@gmail.com>
2022-12-17 11:12:56 +0100

    Milestone 3 - added movies/tv series posters

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,5 +1,5 @@
 <script>
-    // import {store} from '../store.js';
+    import {store} from '../store.js';
 
     export default {
         props: ['movie'],
@@ -13,7 +13,8 @@
                     'de',
                     'fr',
                     'es'
-                ]
+                ],
+                store
             }
         },
         methods: {
@@ -24,6 +25,11 @@
                 this.customURL = this.flagsURL + `${this.movie.original_language}.png`;
 
                 console.log(this.customURL);
+            },
+            posterPath(){
+
+                return this.store.baseUrl+'w185'+this.movie.poster_path;
+
             }
         },
         mounted() {
@@ -35,44 +41,50 @@
 </script>
 
 <template>
-    <!-- <div class="card">
-        <img src="https://www.filmsite.org/posters/exorcist3.jpg" alt="">
-    </div> -->
 
-    
-        <li v-if="movie.media_type == 'tv' || movie.media_type == 'movie'">
-            <a href="#">
+    <li v-if="movie.media_type == 'tv' || movie.media_type == 'movie'">
+        <a href="#">
+
+            <div class="poster">
+
+                <img :src="posterPath()" alt="">
+            
+            </div>
+
+            <div class="info">
 
                 <h3>{{movie.title}}</h3>
-
+                    
                 <h3 v-if="movie.media_type == 'tv'">
                     {{movie.name}}
                 </h3>
-
+    
                 <h5 v-if="movie.original_name !== movie.name">
                     {{movie.original_name}}
                 </h5>
-                
+                    
                 <h5 v-if="movie.original_title !== movie.title">
                     {{movie.original_title}}
                 </h5>
-                
+                    
                 <h5>
-    
-                    <img v-if="flagsOutcomes.includes(movie.original_language)" class="flag" :src="customURL" alt="">
-                    
-                    <img class='flag' src="src/assets/img/missing.png" alt="" v-else>
-                    
-                    -
-                    
-                    {{movie.original_language}}
-                </h5>
-                
-                <h5>{{movie.vote_average}}</h5>
-
-            </a>
         
-        </li>
+                    <img v-if="flagsOutcomes.includes(movie.original_language)" class="flag" :src="customURL" alt="">
+                        
+                    <img class='flag' src="src/assets/img/missing.png" alt="" v-else>
+                        
+                    -
+                        
+                    {{movie.original_language}}
+    
+                </h5>
+                    
+                <h5>{{movie.vote_average}}</h5>
+            </div>
+
+        </a>
+        
+    </li>
     
 </template>
 
@@ -81,6 +93,10 @@
 
     a {
         color: #fff;
+
+        .poster {
+            width: 185px;
+        }
 
         h5 {
 
--- src/store.js
+++ src/store.js
@@ -4,4 +4,5 @@ export const store = reactive ({
     movieList: [],
     apiUrl: 'https://api.themoviedb.org/3/search/multi?api_key=381a82bd8b02866089d3f125535bf53d&language=en-US&page=1&include_adult=false&query=',
     movieQuery: '',
+    baseUrl: 'https://image.tmdb.org/t/p/'
 });
